f7dac358be8422bc3f9841c11aa858b365759b64
Vinicius Gouveia <gouveiavinicius96@gmail.com>
2022-03-02 20:22:27 -0300

    inicio de um JS

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let elementos = document.querySelectorAll(".elemento")
+let numAtomicoElementos = elementos[1]
+let el = numAtomicoElementos.querySelector(".num").textContent
+
+class elementos {
+    
+}
+
+for(i = 0; i < elementos.length; i++) {
+    let elemento = 
+}
+
+
+
+
+
+
+// numAtomico = 50
+// let elemento = elementos[(numAtomico-1)]
+// let nomeElemento = elemento.querySelector(".nome")
+// console.log(nomeElemento.textContent);
+
+
